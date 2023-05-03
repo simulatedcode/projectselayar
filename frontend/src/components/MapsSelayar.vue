e709b0e23a680d3167cd5cc37a6c088b84106b24
ivaaorg <webdev@ivaa-online.org>
2023-05-03 11:59:01 +0700

    init

---
+++ frontend/src/components/MapsSelayar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="h-screen">
+      <div id="map" class="h-full z-10"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import leaflet from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+import { onMounted } from 'vue'
+export default {
+
+  setup () {
+    let map
+    onMounted(() => {
+      map = leaflet.map('map').setView([-6.1363212, 120.5338009], 12)
+      leaflet
+        .tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          attribution:
+            'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+          maxZoom: 19
+        })
+        .addTo(map)
+      leaflet
+        .marker([-6.1013888, 120.4766667])
+        .addTo(map)
+        .bindPopup('<b>Bukit Selayar</b><br> Pulau Selayar.')
+        .openPopup()
+    })
+  }
+}
+</script>
---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <nav>
+      <div class="flex flex-wrap justify-between items-center">
+        <div class="flex">
+          <span class="text-xl font-semibold">Kisah Selayar</span>
+        </div>
+        <div>
+          <ul class="flex gap-4">
+            <li class="bg-gray-100 rounded-full py-2 px-6 shadow">Kisah</li>
+            <li class="bg-gray-100 rounded-full py-2 px-6 shadow">Tentang</li>
+            <li class="bg-gray-100 rounded-full py-2 px-6 shadow">Arsip</li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+  </div>
+</template>
